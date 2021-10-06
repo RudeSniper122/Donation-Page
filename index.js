86fbbc36d6a3570a456dca64eb9291683994b8eb
RudeSniper122 <85300776+RudeSniper122@users.noreply.github.com>
2021-10-06 18:36:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+function s1(){
+  return(
+    x = 1
+    // if (document.getElementById)
+  );
+}
+function myFunction() {
+  var x = s1();
+  document.getElementById('c' + x).classList.toggle("show");
+  document.getElementById('s' + x).classList.toggle("allSideCurveBorder");
+}
+
+// var test = document.
